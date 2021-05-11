59c4644f1fc14cb12bd69bafdec175324fb8df1d
lucasDinizCosta <lucasdinizcostaprofessional@gmail.com>
2021-05-11 16:36:21 -0300

    Ajuste no CSS do App

--- frontend/src/components/form.jsx
+++ frontend/src/components/form.jsx
@@ -18,50 +18,51 @@ export default class Formulario extends React.Component{
                 addressCity: '',
                 addressState: '',
             }}>
-            {() => (
-                <Form>
-                    <div className="form-controller">
+            {(isValid) => (
+                <Form className="form-controller">
+                    <div className="field-controller">
                         <label>Nome: </label>
                         <Field name="name" type="text"/>
                     </div>
-                    <div className="form-controller">
+                    <div className="field-controller">
                         <label>Email: </label>
                         <Field name="email" type="email"/>
                     </div>
-                    <div className="form-controller">
+                    <div className="field-controller">
                         <label>Telefone: </label>
                         <Field name="phone" type="text"/>
                     </div>
-                    <div className="form-controller">
+                    <div className="field-controller">
                         <label>CEP: </label>
                         <Field name="addressZip" type="text"/>
                     </div>
-                    <div className="form-controller">
+                    <div className="field-controller">
                         <label>Logradouro: </label>
                         <Field name="addressStreet" type="text"/>
                     </div>
-                    <div className="form-controller">
+                    <div className="field-controller">
                         <label>Número: </label>
                         <Field name="addressNumber" type="text"/>
                     </div>
-                    <div className="form-controller">
+                    <div className="field-controller">
                         <label>Complemento: </label>
                         <Field name="addressComplement" type="text"/>
                     </div>
-                    <div className="form-controller">
+                    <div className="field-controller">
                         <label>Bairro: </label>
                         <Field name="addressDistrict" type="text"/>
                     </div>
-                    <div className="form-controller">
+                    <div className="field-controller">
                         <label>Cidade: </label>
                         <Field name="addressCity" type="text"/>
                     </div>
-                    <div className="form-controller">
+                    <div className="field-controller">
                         <label>Estado: </label>
                         <Field component="select" name="addressState">
                             <StateList />
                         </Field>
                     </div>
+                    <button type="submit" disabled={!isValid}>Enviar</button>
                 </Form>
             )}   
             </Formik>
